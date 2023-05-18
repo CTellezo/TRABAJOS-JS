ffafbe1c1ff0b93e206ab2f27afee4c7ae1520c8
CTellezo <rt5960369@gmail.com>
2023-05-18 00:46:43 -0500

    nooo me salio

--- SZ/app.js
+++ SZ/app.js
@@ -15,7 +15,20 @@
     let I = parseInt(M.value)
         document.querySelector("#SD").innerHTML=N
    document.querySelector("#Fe").innerHTML=`<h3>Naciste el: ${D.value} de ${M.value} </h3>`
-   if (N = 21 && I<=Febrero) {
-    document.querySelector("#Ho").innerHTML=`<h3></h3>`
-   }
+  let signo=""
+  switch (M.value) {
+    case Enero: signo(N>=20)?"Capricornio": "Acuario"; break;
+    case Febrero: signo(N>=20)?"Acuario": "Piscis"; break;
+    case Marzo: signo(N>=20)?"Piscis": "Aries"; break;
+    case Abril: signo(N>=20)?"Aries": "Tauro"; break;
+    case Mayo: signo(N>=20)?"Tauro": "Geminis"; break;
+    case Junio: signo(N>=20)?"Geminis": "Cancer"; break;
+    case Julio: signo(N>=20)?"Cancer": "Leo"; break;
+    case Agosto: signo(N>=20)?"Leo": "Virgo"; break;
+    case Septiembre: signo(N>=20)?"Virgo": "Libra"; break;
+    case Octubre: signo(N>=20)?"Libra": "Escorpio"; break;
+    case Noviembre: signo(N>=20)?"Escorpio": "Sagitario"; break;
+    case Diciembre: signo(N>=20)?"Sagitario": "Capricornio"; break;
+  }
+  document.querySelector("#Ho").innerHTML=`<h3>Por lo tanto eres: ${signo} </h3>`
  }
